9f3d234439565467920a4ee85a179afcbfb029a7
thelatez <lauri.e.rantala@gmail.com>
2025-10-21 16:32:40 +0300

    Spring pohja

---
+++ kyselypalvelu/src/test/java/hh/kyselypalvelu/KyselypalveluApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.kyselypalvelu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyselypalveluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
