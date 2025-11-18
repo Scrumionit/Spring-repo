0a1fd8672177e6801b899c025d8298611daa3a11
thelatez <lauri.e.rantala@gmail.com>
2025-11-18 16:38:42 +0200

    Korjattu mergevaiheessa peruuntuneet muutokset applicationiin.

--- kyselypalvelu/src/main/java/hh/kyselypalvelu/KyselypalveluApplication.java
+++ kyselypalvelu/src/main/java/hh/kyselypalvelu/KyselypalveluApplication.java
@@ -53,44 +53,35 @@ public class KyselypalveluApplication {
 			 * kysymys2.setKysely(kysely2);
 			 * kysymysRepository.save(kysymys2);
 			 * 
-			 */ KysymysTyyppi avoin = new KysymysTyyppi("Avoin");
+			 */
+			KysymysTyyppi avoin = new KysymysTyyppi("Avoin");
 			KysymysTyyppi monivalinta = new KysymysTyyppi("Monivalinta");
 			kysymysTyyppiRepository.saveAll(List.of(avoin, monivalinta));
 
-			Kysely kysely1 = new Kysely("Hyvinvointikysely",
-					"Kysely opiskelijoiden henkisestä ja fyysisestä hyvinvoinnista", null, "2025-11-13", "2025-12-31");
+			Kysely kysely1 = new Kysely("Hyvinvointikysely","Kysely opiskelijoiden henkisestä ja fyysisestä hyvinvoinnista", null, "2025-11-13", "2025-12-31");
 			kyselyRepository.save(kysely1);
 
 			Kysymys kysymys1 = new Kysymys("Avoin", "Kuinka hyvin nukuit?");
 			kysymys1.setKysely(kysely1);
 			kysymysRepository.save(kysymys1);
 
-			Kysymys kysymys2 = new Kysymys("Avoin", "Mitä harrastat vapaa-ajallasi ja kuinka usein?",
-					Arrays.asList(""));
+			Kysymys kysymys2 = new Kysymys("Avoin", "Mitä harrastat vapaa-ajallasi ja kuinka usein?");
 			kysymys2.setKysely(kysely1);
 			kysymysRepository.save(kysymys2);
 
-			Kysymys kysymys3 = new Kysymys("Avoin",
-					"Onko viikottainen työmäärä opinnoissasi sopiva? Jos ei ole, kerro miksi ja anna ehdotuksia, kuinka tilannetta voisi parantaa.",
-					Arrays.asList(""));
+			Kysymys kysymys3 = new Kysymys("Avoin", "Onko viikottainen työmäärä opinnoissasi sopiva? Jos ei ole, kerro miksi ja anna ehdotuksia, kuinka tilannetta voisi parantaa.");
 			kysymys3.setKysely(kysely1);
 			kysymysRepository.save(kysymys3);
 
-			Kysymys kysymys4 = new Kysymys("Avoin",
-					"Miten koet sosiaalisen elämäsi opiskelijana? Onko sinulla tarpeeksi aikaa ystäville ja perheelle?",
-					Arrays.asList(""));
+			Kysymys kysymys4 = new Kysymys("Avoin", "Miten koet sosiaalisen elämäsi opiskelijana? Onko sinulla tarpeeksi aikaa ystäville ja perheelle?");
 			kysymys4.setKysely(kysely1);
 			kysymysRepository.save(kysymys4);
 
-			Kysymys kysymys5 = new Kysymys("Avoin",
-					"Miten arvioisit mielialaasi viimeisen kuukauden aikana? Onko sinulla ollut stressiä, ahdistusta tai muita mielialan vaihteluita?",
-					Arrays.asList(""));
+			Kysymys kysymys5 = new Kysymys("Avoin", "Miten arvioisit mielialaasi viimeisen kuukauden aikana? Onko sinulla ollut stressiä, ahdistusta tai muita mielialan vaihteluita?");
 			kysymys5.setKysely(kysely1);
 			kysymysRepository.save(kysymys5);
 
-			Kysymys kysymys6 = new Kysymys("Avoin",
-					"Onko sinulla riittävästi tukea opinnoissasi, kuten opettajilta, opiskelutovereilta tai muilta tukipalveluilta?",
-					Arrays.asList(""));
+			Kysymys kysymys6 = new Kysymys("Avoin", "Onko sinulla riittävästi tukea opinnoissasi, kuten opettajilta, opiskelutovereilta tai muilta tukipalveluilta?");
 			kysymys6.setKysely(kysely1);
 			kysymysRepository.save(kysymys6);
 
