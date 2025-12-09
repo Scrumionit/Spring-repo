e9180ccda42517af8cc1a2f028d0242eb0a49fb8
bhq718 <bhq718@myy.haaga-helia.fi>
2025-12-09 14:26:32 +0200

    poistettu testidata rivit koodista

--- kyselypalvelu/src/main/java/hh/kyselypalvelu/KyselypalveluApplication.java
+++ kyselypalvelu/src/main/java/hh/kyselypalvelu/KyselypalveluApplication.java
@@ -21,40 +21,7 @@ public class KyselypalveluApplication {
 	public CommandLineRunner demo(KyselyRepository kyselyRepository, KysymysRepository kysymysRepository,
 			KysymysTyyppiRepository kysymysTyyppiRepository) {
 		return (args) -> {
-			// Testidata voidaan lisätä tähän tarvittaessa
-			/*
-			 * TESTIDATA:
-			 * Kysely kysely1 = new Kysely("Asiakastyytyväisyyskysely",
-			 * "Kysely asiakkaiden tyytyväisyydestä palveluihimme", null, "2023-01-01",
-			 * "2023-01-31");
-			 * Kysely kysely2 = new Kysely("Työtyytyväisyyskysely",
-			 * "Kysely työntekijöiden tyytyväisyydestä työympäristöön", null, "2023-02-01",
-			 * "2023-02-28");
-			 * Kysely kysely3 = new Kysely("Tuotepalaute",
-			 * "Kysely tuotteidemme laadusta ja toimivuudesta", null, "2023-03-01",
-			 * "2023-03-31");
-			 * kyselyRepository.save(kysely1);
-			 * kyselyRepository.save(kysely2);
-			 * kyselyRepository.save(kysely3);
-			 * 
-			 * Kysymys kysymys1 = new Kysymys("Monivalinta",
-			 * "Kuinka tyytyväinen olet palveluumme?", Arrays.asList("Erittäin tyytyväinen",
-			 * "Tyytyväinen", "Neutraali", "Tyytymätön", "Erittäin tyytymätön"));
-			 * kysymys1.setKysely(kysely1);
-			 * kysymysRepository.save(kysymys1);
-			 * 
-			 * Kysymys kysymys3 = new Kysymys("Monivalinta",
-			 * "Kuinka usein käytät tuotteitamme?", Arrays.asList("Päivittäin",
-			 * "Viikoittain", "Kuukausittain", "Harvemmin"));
-			 * kysymys3.setKysely(kysely1);
-			 * kysymysRepository.save(kysymys3);
-			 * 
-			 * Kysymys kysymys2 = new Kysymys("Avoin",
-			 * "Miten voisimme parantaa palveluamme?", null);
-			 * kysymys2.setKysely(kysely2);
-			 * kysymysRepository.save(kysymys2);
-			 * 
-			 */
+		
 			KysymysTyyppi avoin = new KysymysTyyppi("Avoin");
 			KysymysTyyppi monivalinta = new KysymysTyyppi("Monivalinta");
 			kysymysTyyppiRepository.saveAll(List.of(avoin, monivalinta));
@@ -138,7 +105,7 @@ public class KyselypalveluApplication {
 			kysymys10.setKysely(kysely1);
 			kysymysRepository.save(kysymys10);
 
-			
+
 		};
 	}
 }
