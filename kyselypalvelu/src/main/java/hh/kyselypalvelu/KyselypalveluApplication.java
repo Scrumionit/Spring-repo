6ccca4547ca97c2b7086da64ed5720608cfff834
bhq718 <bhq718@myy.haaga-helia.fi>
2025-12-09 14:27:06 +0200

    poistettu turha import

--- kyselypalvelu/src/main/java/hh/kyselypalvelu/KyselypalveluApplication.java
+++ kyselypalvelu/src/main/java/hh/kyselypalvelu/KyselypalveluApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.HashSet;
+
 import java.util.List;
 
 import hh.kyselypalvelu.domain.*;
